7e9e1b79dc1bafcc629b9609de12b4d1f8dab156
Phil Chin <phil.s.chin@outlook.com>
2023-07-22 09:09:57 -0400

    access remotely worked.

---
+++ BlazorToDoList.App/Data/BurninInfo.cs
@@ -0,0 +1,15 @@
+﻿using MyProject;
+
+using System;
+
+namespace BlazorToDoList.App.Data
+{
+    public class BurninInfo
+    {
+        public DateTime startdate { get; set; }
+        public string serial { get; set; }
+        public int NumberOfSamples { get; set; }
+        public LaserLogMsg4s laserLogMsg4S { get; set; }
+            
+    }
+}
---
+++ BlazorToDoList.App/Data/BurninInfoService.cs
@@ -0,0 +1,74 @@
+﻿using MsgLibrary;
+
+using MyProject;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorToDoList.App.Data
+{
+    
+    
+    public class BurninInfoService
+    {
+        NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        const string filename = @".\data\demo.db";
+        private List<string> _serialNumebrs = new List<string>();
+
+        public List<string>  serials { get=> _serialNumebrs; }
+
+        public BurninInfoService()
+        {
+            Debug.Print("burninfoService");
+        }
+
+        public async Task<double[]> GetPowerAsync()
+        {
+            logger.Info("GetPowerAsync");
+
+            try
+            {
+                SaveToSqlite.ConnectionString = filename;
+                //make sure the file exist.
+                if (!File.Exists(filename))
+                    throw new FileNotFoundException("File not found", filename);
+
+                // get all the serials.
+                List<string> serialNumbers = SaveToSqlite.GetSerialsMsg4();
+
+                double[] doubleArray = new double[serialNumbers.Count];
+                const int arraysize = 5;
+
+                for (int i = 0; i < serialNumbers.Count; i++)
+                {
+
+                    doubleArray[i] = SaveToSqlite.GetLastPwr(serialNumbers[i]);
+                }
+
+                _serialNumebrs = serialNumbers;
+                return doubleArray;
+            }
+            catch (Exception ex)
+            {
+
+                logger.Error(ex, "GetPowerAsync");
+                throw;
+            }
+            
+            
+
+        }
+
+        public async Task<laserInfo[]> GetLaserInfoAsync()
+        {
+            laserInfo[] laserInfos = new laserInfo[5];
+            return  laserInfos;
+        }
+
+        
+    }
+}
